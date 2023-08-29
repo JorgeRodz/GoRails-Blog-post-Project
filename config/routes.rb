4395105de1cf164e3d36c78772facdfac23b4f75
Jorge Rodriguez <jarf4321@gmail.com>
2023-08-29 03:26:10 -0600

    Add show functionality to display individuals blog posts
    - In index page a link to show individuals blog post
    - Create route, controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'blog_posts#index'
+  #----- Blog Posts routes -----
+  # :id is a dynamic segment, which will be replaced with the ID of the blog post
+
+  # "as: new_blog_post" gives the route a name, which can be used in the view helper as 'new_blog_post_path'
+
+  # "as: blog_post" gives the route a name, which can be used in the view helper as 'blog_post_path'
+  get '/blog_posts/:id', to: 'blog_posts#show', as: :blog_post
 end
