92a80e3419d6991e1d123aaa393092ed40e5c43f
Jorge Rodriguez <jarf4321@gmail.com>
2023-09-02 13:18:01 -0600

    Add form view
    - Add 'new' method in the blog_posts_controller
    - Add 'new' view to display form
      - Add flash message in order to display model validations errors
    - Add 'new' route to visualize the form

--- config/routes.rb
+++ config/routes.rb
@@ -3,10 +3,13 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'blog_posts#index'
+
   #----- Blog Posts routes -----
   # :id is a dynamic segment, which will be replaced with the ID of the blog post
 
   # "as: new_blog_post" gives the route a name, which can be used in the view helper as 'new_blog_post_path'
+  # the 'new' route will always be above the 'show' route, otherwise the 'show' route will be used instead of the 'new' route
+  get '/blog_posts/new', to: 'blog_posts#new', as: :new_blog_post
 
   # "as: blog_post" gives the route a name, which can be used in the view helper as 'blog_post_path'
   get '/blog_posts/:id', to: 'blog_posts#show', as: :blog_post
